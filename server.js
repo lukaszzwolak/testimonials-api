e9c9400a8abcca77a65be5d679b64ef3682e20e1
Łukasz Zwolak <145567355+lukaszzwolak@users.noreply.github.com>
2025-07-28 13:47:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ mongoose
       useUnifiedTopology: true,
     }
   )
-  .then(() => console.log("Connected to MongoDB ✅"))
-  .catch((err) => console.error("Error connecting to MongoDB ❌:", err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 const app = express();
 
 // middleware
